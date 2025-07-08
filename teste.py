8d0961f3b7493794d4204f814dad6fbccc29b221
Raylan Araujo <raylanra@gmail.com>
2025-07-08 04:38:27 -0300

    codificado as classes Cliente, PessoaFisica e Conta

---
+++ teste.py
@@ -0,0 +1,95 @@
+class Cliente:
+    def __init__(self, endereco):
+        self._endenreco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta) #adiciona a conta recebida por parâmetro no array de contas 
+
+
+class PessoaFisica(Cliente): #classe extendida da classe Cliente
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        super().__init__(endereco) #chamou o construtor da classe pai Cliente com o argumento que ele pede
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+
+class Conta:
+    def __init__(self, numero, cliente, historico):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "0001"
+        self._cliente = cliente
+        self._historico = Historico() #é do tipo classe. Uma conta tem um histórico e um histórico pertence a uma conta.
+
+    @classmethod
+    def nova_conta(cls, cliente, numero): #recebe como argumento Cliente e número. #método de fábrica que cria algo. Esse método vai receber um cliente e o número e tem que retornar o objeto conta. 
+        return cls(numero, cliente) # retorna uma instância de conta
+    
+    #abaixo foi criado propriedades para acessar os dados 
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @property
+    def numero(self):
+        return self._numero
+
+    @property    
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+
+
+    def sacar(self, valor): #recebe um valor float e retornar um booleano
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print("\n\033[31m[ERRO!] - Saldo insuficiente.\033[m")
+        
+        elif valor > 0:
+            self._saldo -= valor
+            print("\n\033[32mSaque realizado com sucesso.\033[m")
+            return True
+        
+        else:
+            print("\n\033[31mDigite um valor válido.\033[m")
+            return False
+            
+
+    def depositar(self, valor): #recebe um valor que é float e retorna um booleano. A intencao do é saber se a oprecao aconteceu com sucesso ou falha. 
+        if valor > 0: #verificar se o valor informado é maior que 0
+            self._saldo += saldo
+            print("\n\033[32mDepósito realizado com sucesso.\033[m")
+        else:
+            print("\n\033[31mDigite um valor válido.\033[m")
+            return False
+        
+        return True
+
+
+
+class ContaCorrente(Conta): #tem tudo que a conta mae tem + o limite
+    def __init__(self, limite, limite_saques, **kw):
+        super().__init__(**kw)
+        self._limite = limite
+        self._limite_saques = limite_saques
+
+
+class Historico():
+    def adicionar_transacao(transacao):
+        pass
+
+#minuto 2:30
