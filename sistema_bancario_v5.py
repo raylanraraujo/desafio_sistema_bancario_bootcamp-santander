311ace731d35ea2d5adc74cdf85003606dc9ebb5
Raylan Araujo <raylanra@gmail.com>
2025-07-16 05:27:32 -0300

    corrigido os segundos na data da transacao

--- sistema_bancario_v5.py
+++ sistema_bancario_v5.py
@@ -160,7 +160,7 @@ class Historico():
             {
                 "tipo": transacao.__class__.__name__,
                 "valor": transacao.valor,
-                "data": datetime.now().strftime("%d-%m-%Y  %H:%M:%s")
+                "data": datetime.now().strftime("%d-%m-%Y  %H:%M:%S")
             }
         )
 
